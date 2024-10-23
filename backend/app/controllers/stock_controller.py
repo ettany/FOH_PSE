b0090ee9907183d42268cacd41b6e9e6acf443ae
ettany <67234683+ettany@users.noreply.github.com>
2024-10-22 22:42:53 -0400

    commit logs and Tamsyn code

--- backend/app/controllers/stock_controller.py
+++ backend/app/controllers/stock_controller.py
@@ -1,33 +1,28 @@
 import os
-import requests
 from flask import Blueprint, jsonify, request
+import yfinance as yf
 
+from yahooquery import Screener
 stock_bp = Blueprint('stock', __name__)
 
 API_KEY = os.getenv('ALPHA_VANTAGE_API_KEY')  # Load your API key from environment variables
 
 @stock_bp.route('/', methods=['GET'])
 def get_stocks():
-    # symbols = request.args.get('symbols')  # Fetch symbols from query parameters
-    symbols = request.args.get('symbols', 'AAPL,GOOGL,TSLA')  # Default to these symbols if none are provided
-    if not symbols:
-        return jsonify({'error': 'No stock symbols provided'}), 400
-    
-    symbols_list = symbols.split(',')  # Split symbols by comma if multiple are provided
-    stocks_data = {}
-    
-    for symbol in symbols_list:
-        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=5min&apikey={API_KEY}'
-        response = requests.get(url)
-        data = response.json()
-        
-        if 'Time Series (5min)' in data:
-            latest_time = next(iter(data['Time Series (5min)']))
-            stocks_data[symbol] = {
-                'name': symbol,  # Modify this to get actual company names if 
-                'price': data['Time Series (5min)'][latest_time]['1. open']  # Fetch the latest price
-            }
-        else:
-            stocks_data[symbol] = {'name': symbol, 'price': 'N/A'}  # Handle errors gracefully
+    stocks = {}
+    tickers_file_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),"..", "logs", "tickers.txt"
+    )
 
-    return jsonify(stocks_data)
+    with open(tickers_file_path, "r") as file:
+        tickers = [line.strip() for line in file.readlines()]
+
+    for ticker in tickers:
+        stock = yf.Ticker(ticker)
+        stock_info = stock.info
+        stocks[ticker] = {
+            "name": stock_info.get("shortName"),
+            "price": stock_info.get("currentPrice"),
+        }
+
+    return jsonify(stocks)
