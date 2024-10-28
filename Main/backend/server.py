db7e51d9185e52468d835c97a44e95466a529004
Tamsyn Evezard <93171379+TamsynE@users.noreply.github.com>
2024-10-28 13:00:30 -0400

    Update server.py

--- Main/backend/server.py
+++ Main/backend/server.py
@@ -2,17 +2,20 @@ import os
 from flask import Flask, render_template, request, redirect, url_for
 import yfinance as yf
 import db
-from flask import Flask, render_template, redirect, url_for, session
+from flask import Flask, render_template, redirect, url_for, session, flash
+from chart import stockChart_blueprint
 
 
 def create_app():
     app = Flask(
         __name__,
         template_folder=os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "..", "frontend", "templates"
+            os.path.dirname(os.path.abspath(__file__)
+                            ), "..", "frontend", "templates"
         ),
         static_folder=os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), "..", "frontend", "static"
+            os.path.dirname(os.path.abspath(__file__)
+                            ), "..", "frontend", "static"
         ),
     )
 
@@ -20,6 +23,8 @@ def create_app():
     app.config["DATABASE"] = os.path.join(
         os.path.dirname(os.path.abspath(__file__)), "database.db"
     )
+
+    app.register_blueprint(stockChart_blueprint)
 
     # Initialize the database commands
     db.init_app(app)
@@ -39,21 +44,34 @@ def register():
         username = request.form["username"]
         password = request.form["password"]
 
-        # Insert user into the database
+        # Check if the username already exists
         db_conn = db.get_db()
+        existing_user = db_conn.execute(
+            "SELECT * FROM user WHERE username = ?",
+            (username,),
+        ).fetchone()
+
+        if existing_user:
+            flash("Username already taken. Please choose a different one.")
+            return redirect(url_for("register"))
+
+        # Insert user into the database
         db_conn.execute(
             "INSERT INTO user (username, password, totalCash) VALUES (?, ?, ?)",
-            (username, password, 10000),
+            (username, password, 100000),
         )
         db_conn.commit()
 
-        return redirect(url_for("index"))
+        return redirect(url_for("login"))
+
     return render_template("register.html")
 
 
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    username = session.get("username")
+    totalCash = session.get("totalCash")
+    return render_template("index.html", username=username, totalCash=totalCash)
 
 
 @app.route("/trade")
@@ -66,7 +84,7 @@ def trade():
     with open(tickers_file_path, "r") as file:
         tickers = [line.strip() for line in file.readlines()]
 
-    for ticker in tickers:
+    for ticker in tickers[:5]:
         stock = yf.Ticker(ticker)
         stock_info = stock.info
         stocks[ticker] = {
@@ -74,8 +92,23 @@ def trade():
             "price": stock_info.get("currentPrice"),
         }
 
-    return render_template("trade.html", stocks=stocks)
+    searched_stock = None
 
+    # Searching for a specific stock
+    if request.method == "POST":
+        search_ticker = request.form["stock_search"].upper()
+        stock = yf.Ticker(search_ticker)
+        stock_info = stock.info
+
+        # Check if the stock data exists and is valid
+        if "shortName" in stock_info and "currentPrice" in stock_info:
+            searched_stock = {
+                "ticker": search_ticker,
+                "name": stock_info["shortName"],
+                "price": stock_info["currentPrice"],
+            }
+
+    return render_template("trade.html", stocks=stocks, searched_stock=searched_stock)
 
 
 @app.route("/log")
@@ -86,27 +119,39 @@ def log():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        session['username'] = request.form["username"]
+        username = request.form["username"]
         password = request.form["password"]
 
         # Check the credentials against the database
-        db_conn = db.get_db()
-        user = db_conn.execute(
-            "SELECT * FROM user WHERE username = ? AND password = ?",
-            (session['username'], password),
-        ).fetchone()
+        try:
+            db_conn = db.get_db()
+            user = db_conn.execute(
+                "SELECT * FROM user WHERE username = ? AND password = ?",
+                (username, password),
+            ).fetchone()
 
-        if user:
-            return redirect(url_for("trade"))
-        else:
-            return "Invalid credentials"
+            if user:
+                session["username"] = username
+                if username == "administration":
+                    return redirect(url_for("admin"))
+                else:
+                    session["totalCash"] = user["totalCash"]
+                    return redirect(url_for("index"))
+            else:
+                flash("Invalid Credentials.")
+                return redirect(url_for("login"))
+
+        except Exception as e:
+            print(f"Error: {e}")
+            return "Database connection error"
 
     return render_template("login.html")
 
+
 @app.route("/buy", methods=["POST"])
 def buy():
-    
-    ticker = request.form["ticker"] 
+
+    ticker = request.form["ticker"]
     numShares = int(request.form["numShares"])
 
     stock = yf.Ticker(ticker)
@@ -115,24 +160,29 @@ def buy():
     totalCost = numShares * stock_info.get("currentPrice")
 
     db_conn = db.get_db()
-    id = db_conn.execute("SELECT id FROM user WHERE username = ?", (session['username'])).fetchone()
-    cash = db_conn.execute("SELECT totalCash FROM user WHERE username = ?", (session['username'])).fetchone()
+    id = db_conn.execute(
+        "SELECT id FROM user WHERE username = ?", (session['username'])).fetchone()
+    cash = db_conn.execute(
+        "SELECT totalCash FROM user WHERE username = ?", (session['username'])).fetchone()
     if (totalCost <= cash['totalCash']):
-        #Set total cash to be lower
-        db_conn.execute("UPDATE user SET totalCash = totalCash - ? WHERE username = ?", (totalCost, session['username']))
-        #insert buying action into event log
+        # Set total cash to be lower
+        db_conn.execute("UPDATE user SET totalCash = totalCash - ? WHERE username = ?",
+                        (totalCost, session['username']))
+        # insert buying action into event log
         db_conn.execute("INSERT INTO eventLog (id, eventName, stockBought) VALUES (?, ?, ?)",
-            (id['id'], 'Bought', ticker))
-        #insert stocks into portfolio
-        db_conn.execute("INSERT INTO portfolio (ticker, numShares, id) VALUES(?,?,?)", (ticker, numShares, id['id']))
+                        (id['id'], 'Bought', ticker))
+        # insert stocks into portfolio
+        db_conn.execute(
+            "INSERT INTO portfolio (ticker, numShares, id) VALUES(?,?,?)", (ticker, numShares, id['id']))
         return redirect(url_for("trade"))
     else:
         "Error"
 
+
 @app.route("/sell", methods=["POST"])
 def sell():
-    
-    ticker = request.form["ticker"] 
+
+    ticker = request.form["ticker"]
     numShares = int(request.form["numShares"])
 
     stock = yf.Ticker(ticker)
@@ -141,27 +191,37 @@ def sell():
     totalProfit = numShares * stock_info.get("currentPrice")
 
     db_conn = db.get_db()
-    
-    id = db_conn.execute("SELECT id FROM user WHERE username = ?", (session['username'])).fetchone()
 
-    currentShares = db_conn.execute("SELECT numShares FROM portfolio WHERE id = ? AND ticker = ?", (id, ticker))
-    
+    id = db_conn.execute(
+        "SELECT id FROM user WHERE username = ?", (session['username'])).fetchone()
+
+    currentShares = db_conn.execute(
+        "SELECT numShares FROM portfolio WHERE id = ? AND ticker = ?", (id, ticker))
+
     if (currentShares['numShares'] >= numShares):
-        #Set total cash to be higher
-        db_conn.execute("UPDATE user SET totalCash = totalCash + ? WHERE id = ?", (totalProfit, id['id']))
-        #insert selling action into event log
-        db_conn.execute("INSERT INTO eventLog (id, eventName, stockBought) VALUES (?, ?, ?)", (id['id'], 'Sold', ticker))
-        #remove number of shares from portfolio table -- maybe readjust to only print stocks with a number of shares greater than zero?
-        db_conn.execute("UPDATE portfolio SET numShares = numShares - ? WHERE id = ? AND ticker = ?", (numShares, id['id'], ticker))
+        # Set total cash to be higher
+        db_conn.execute(
+            "UPDATE user SET totalCash = totalCash + ? WHERE id = ?", (totalProfit, id['id']))
+        # insert selling action into event log
+        db_conn.execute(
+            "INSERT INTO eventLog (id, eventName, stockBought) VALUES (?, ?, ?)", (id['id'], 'Sold', ticker))
+        # remove number of shares from portfolio table -- maybe readjust to only print stocks with a number of shares greater than zero?
+        db_conn.execute(
+            "UPDATE portfolio SET numShares = numShares - ? WHERE id = ? AND ticker = ?", (numShares, id['id'], ticker))
         return redirect(url_for("trade"))
     else:
         "Error"
 
 
+@app.route("/admin")
+def admin():
+    username = session.get("username")
+    return render_template("admin.html", username=username)
 
-    
 
-
+@app.route("/visualization")
+def visualization():
+    return render_template("visualization.html")
 
 
 @app.route("/logout")
